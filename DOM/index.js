8c0e86406132d5e5c32e316d207cc556522854be
vjohndo <v.john.do@outlook.com>
2023-02-07 12:59:54 +1100

    [S11] DOM - class lists and attributes

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,4 @@
-document.querySelector("ul").lastElementChild.innerHTML = "LAST";
+document.querySelector("ul a").setAttribute("href","http://www.bing.com")
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
