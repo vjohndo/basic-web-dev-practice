264ad027e811b2573fb798770aa2704fa72eede6
vjohndo <v.john.do@outlook.com>
2023-02-07 23:09:27 +1100

    [S19] express server nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    console.log(req);
+    res.send("<h1><em>Hello World!</em></h1>");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact me at: email@gmail.com");
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is info about me");
 })
 
 app.listen(port, () => {
