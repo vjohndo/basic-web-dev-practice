b31caab34adcc60c4ffd8f935144a1445757eccb
vjohndo <v.john.do@outlook.com>
2023-02-09 15:23:14 +1100

    [S22] redirecting forms and EJS

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -8,16 +8,44 @@ app.set("view engine", "ejs"); // Tells app to use ejs
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+const toDoList = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get("/", (req, res) => {
     const today = new Date();
-
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var currentDayIndex = today.getDay();
-    var day = weekday[currentDayIndex];
     
-    res.render("list", {day: day});
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    const day = today.toLocaleDateString("en-US", options);
+    
+    res.render("list", {listTitle: day, toDoList: toDoList});
+})
+
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work", toDoList: workItems});
+})
+
+app.post("./work", (req, res) => {
+    let item = req.body.item;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.post("/", (req, res) => {
+
+    const item = req.body.item;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        toDoList.push(item);
+        res.redirect("/");
+    }
 })
 
 app.listen(port, () => {
