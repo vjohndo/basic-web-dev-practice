490febc0df59afd3271fc4ae8535de99dad35c5f
vjohndo <v.john.do@outlook.com>
2023-02-09 12:06:28 +1100

    [S21] passing variables to ejs markers

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,17 +7,16 @@ const port = 3000;
 app.set("view engine", "ejs"); // Tells app to use ejs
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
 app.get("/", (req, res) => {
     const today = new Date();
-    var currentDay = today.getDay();
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Hello World!</h1>");
-        res.send();
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var currentDayIndex = today.getDay();
+    var day = weekday[currentDayIndex];
     
+    res.render("list", {day: day});
 })
 
 app.listen(port, () => {
