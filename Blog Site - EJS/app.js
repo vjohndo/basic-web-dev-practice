46e73ca3882b86948a1cc20639d0c07e217a338e
vjohndo <v.john.do@outlook.com>
2023-02-09 18:09:30 +1100

    [S23] EJS + Node Blog Project Complete

--- Blog Site - EJS/app.js
+++ Blog Site - EJS/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,24 +11,54 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home", {
+    content: homeStartingContent, 
+    posts: posts
+});
+})
 
+app.get("/about", (req, res) => {
+  res.render("about", {content: aboutContent});
+})
 
+app.get("/contact", (req, res) => {
+  res.render("contact", {content: contactContent});
+})
 
+app.get("/compose", (req, res) => {
+  res.render("compose")
+})
 
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
+  console.log(post.body.slice(100));
+  posts.push(post);
+  res.redirect("/");
+})
 
-
-
-
-
-
-
-
+app.get("/posts/:postName", (req, res) => {
+  const requestTitle = lodash.lowerCase(req.params.postName);
+  posts.forEach( (post) => {
+    const storedTitle = lodash.lowerCase(post.title);
+    if (storedTitle === requestTitle) {
+      res.render("post", {post: post});
+    } else {
+      console.log("Not a match")
+    }
+  })
+})
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port http://localhost:3000");
 });
