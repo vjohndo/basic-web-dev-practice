df808e941cc4a4418d2c15a2dcd00748c4e24da2
vjohndo <v.john.do@outlook.com>
2023-02-10 21:28:35 +1100

    [S30] Blog website mongodb init

--- Blog Site - EJS/app.js
+++ Blog Site - EJS/app.js
@@ -1,13 +1,46 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1/blogDB", { useNewUrlParser: true });
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contentSchema = new mongoose.Schema({
+	title: String,
+	content: String
+})
+
+const Content = mongoose.model("Content", contentSchema);
+const Blog = mongoose.model("Blog", contentSchema);
+
+Content.find({}, (err, foundContent) => {
+	if (err) {
+		console.log(err);
+	} else if (foundContent.length === 0) {
+		const homeContent = new Content({
+			title: "Home",
+			content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+		});
+		const aboutContent = new Content({
+			title: "About",
+			content: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+		});
+		const contactContent = new Content({
+			title: "Contact",
+			content: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
+		});
+		const startingContents = [homeContent, aboutContent, contactContent];
+		Content.insertMany(startingContents, (err) => {
+			if (err) {
+				console.log(err)
+			} else {
+				console.log("Adding content in db");
+			}
+		});
+	} else {
+		console.log("Existing content in db");
+	}
+})
 
 const app = express();
 
@@ -15,50 +48,51 @@ const posts = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    content: homeStartingContent, 
-    posts: posts
-});
+	
+	res.render("home", {
+		content: homeStartingContent,
+		posts: posts
+	});
 })
 
 app.get("/about", (req, res) => {
-  res.render("about", {content: aboutContent});
+	res.render("about", { content: aboutContent });
 })
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {content: contactContent});
+	res.render("contact", { content: contactContent });
 })
 
 app.get("/compose", (req, res) => {
-  res.render("compose")
+	res.render("compose")
 })
 
 app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.postTitle,
-    body: req.body.postBody
-  }
-  console.log(post.body.slice(100));
-  posts.push(post);
-  res.redirect("/");
+	const post = {
+		title: req.body.postTitle,
+		body: req.body.postBody
+	}
+	console.log(post.body.slice(100));
+	posts.push(post);
+	res.redirect("/");
 })
 
 app.get("/posts/:postName", (req, res) => {
-  const requestTitle = lodash.lowerCase(req.params.postName);
-  posts.forEach( (post) => {
-    const storedTitle = lodash.lowerCase(post.title);
-    if (storedTitle === requestTitle) {
-      res.render("post", {post: post});
-    } else {
-      console.log("Not a match")
-    }
-  })
+	const requestTitle = lodash.lowerCase(req.params.postName);
+	posts.forEach((post) => {
+		const storedTitle = lodash.lowerCase(post.title);
+		if (storedTitle === requestTitle) {
+			res.render("post", { post: post });
+		} else {
+			console.log("Not a match")
+		}
+	})
 })
 
-app.listen(3000, function() {
-  console.log("Server started on port http://localhost:3000");
+app.listen(3000, function () {
+	console.log("Server started on port http://localhost:3000");
 });
