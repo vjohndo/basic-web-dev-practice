e49d9667bb57115d6ebe580f3ea24f0f7174a23f
vjohndo <v.john.do@outlook.com>
2023-02-12 11:19:48 +1100

    [S32] Authentication site

---
+++ Secrets - authentication site/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.listen(3000, () => {
+    console.log("Server listening on http://localhost:" + 3000);
+})
