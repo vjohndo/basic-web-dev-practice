e0ce28d78c048ba233261c644b5a48d2abe777d0
vjohndo <v.john.do@outlook.com>
2023-02-13 00:18:51 +1100

    [S32] authentication - logins as plain text

--- Secrets - authentication site/app.js
+++ Secrets - authentication site/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/secretsDB", {useNewUrlParser: true})
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,16 +18,50 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
-app.get("/login", (req, res) => {
-    res.render("login");
-})
+app.route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post((req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
 
-app.get("/register", (req, res) => {
-    res.render("register");
-})
+        User.findOne({email: username}, (err, foundUser) => {
+            if (err) {
+                console.log(err)
+            } else if (foundUser && foundUser.password === password) {
+                res.render("secrets");
+            }
+        })
+    });
+
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        const email = req.body.username;
+        const password = req.body.password;
+        
+        console.log(email, password);
+
+        const newUser = new User({
+            email: email,
+            password: password
+        });
+
+        newUser.save((err) => {
+            if (!err) {
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        })
+    });
+
 
 app.listen(3000, () => {
     console.log("Server listening on http://localhost:" + 3000);
-})
+});
