dc0fbced84cc08105cdf7e186584bc2dbab21923
vjohndo <v.john.do@outlook.com>
2023-02-14 10:49:02 +1100

    [S33] Intro to JSX save

---
+++ React/code-sandbox-saves/01-intro/src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+      <h1>Hello World!</h1>
+      <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root"));
