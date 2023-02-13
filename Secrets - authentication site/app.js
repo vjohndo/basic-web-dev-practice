c56b5c47786cc731d490d2af0927ee6aa3416784
vjohndo <v.john.do@outlook.com>
2023-02-13 22:27:39 +1100

    [S32] Authentication - moving on

--- Secrets - authentication site/app.js
+++ Secrets - authentication site/app.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); // Passport-local is just a dependency for passport-local-mongoose
 GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+const findOrCreate = require("mongoose-findorcreate"); // Package to handle passport's findorcreate pseudo code
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,51 +21,75 @@ app.use(session({
     saveUninitialized: false
 })); // Config the session
 
-app.use(passport.initialize()); // Tell app to use and initialise passport
-app.use(passport.session()); // Tell passport deal with sessions
+app.use(passport.initialize()); // Middleware: Tell app to use and initialise passport
+app.use(passport.session()); // Middleware: Tell passport deal with persistent sessions
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/secretsDB", { useNewUrlParser: true })
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+    username: {type: String, unique: true},
+    password: String,
+    provider: String,
+    email: String
 });
-userSchema.plugin(passportLocalMongoose); // This is how we add in the Mongoose passport
-userSchema.plugin(findOrCreate);
+userSchema.plugin(passportLocalMongoose, {usernameField: "username"}); // This is how we add in the Mongoose passport
+userSchema.plugin(findOrCreate); // Package to handle passport's findorcreate pseudo code
 
 const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy()); // Strategy is to authenticate using username and password
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(User.createStrategy()); // Strategy is the method to authenticate. We must configure passport.
 
-//,userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+// Copied from passport js docs:
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+//As google plus is sunsetting: ,userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+  function(accessToken, refreshToken, profile, cb) { // Google sends back access token
+        User.findOrCreate(
+            { googleId: profile.id },
+            { provider: "google", email: profile._json.email },
+            function (err, user) { // findOrCreate is actually pseudo code
+                console.log(profile);
+                return cb(err, user);
+            }
+        );
+    }
 ));
 
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/auth/google", (req, res) => {
-    passport.authenticate("google", {scope: ["profile"]})
-});
+// Fixed hanging login. Make sure to use the documentation.
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
-app.get("/auth/google/callback", (req, res) => {
-    passport.authenticate("google", { failureRedirect: "/login"}),
+app.get("/auth/google/secrets", 
+    passport.authenticate('google', { failureRedirect: '/login' }), // Authenticaed on googles end, now authenticate locally
     (req, res) => {
-        res.redirect("/");
-    }
+      // Successful authentication, redirect home.
+        res.redirect('/secrets');
 });
 
 app.route("/login")
@@ -78,11 +102,11 @@ app.route("/login")
             password: req.body.password
         })
 
-        req.login(user, (err) => { // This is from the passport package
+        req.login(user, (err) => { // This is from the passport package. Req provided for authetication
             if (err) {
                 console.log(err);
             } else {
-                passport.authenticate("local")(req, res, () => { // uses passport
+                passport.authenticate("local")(req, res, () => { // uses passport to autheticate
                     res.redirect("/secrets");
                 });
             }
@@ -99,13 +123,25 @@ app.get("/secrets", (req, res) => {
     }
 })
 
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login")
+    }
+})
+
 app.route("/register")
     .get((req, res) => {
         res.render("register");
     })
     .post((req, res) => {
         // Method "register" is from passport-local-mongoose
-        User.register({username: req.body.username}, req.body.password, (err, user) => {
+
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.register({username: username, email: username, provider: 'local'}, password, (err, user) => {
             if (err) {
                 console.log(err);
                 res.redirect("/register");
