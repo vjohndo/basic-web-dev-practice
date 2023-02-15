1105db3f1a124a6563c732f7bf458eb2af921200
vjohndo <v.john.do@outlook.com>
2023-02-15 17:09:40 +1100

    [S33] React forms practice

--- React/12-react-forms/src/components/App.jsx
+++ React/12-react-forms/src/components/App.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [headingText, setHeadingText] = useState("");
+
+    function handleChange(event) {
+        console.log(event.target.value);
+        setName(event.target.value);
+    };
+
+    function handleClick(event) {
+        setHeadingText(name);
+        event.preventDefault();
+    };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleClick}>
+                <h1>Hello, {headingText}</h1>
+                <input
+                    type="text"
+                    placeholder="What's your name?"
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
