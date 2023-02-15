b89812c852fce0e513c1b2b94aa96b8065f38d0f
vjohndo <v.john.do@outlook.com>
2023-02-15 16:46:14 +1100

    [S33] event handling practice

--- React/11-event-handling/src/components/App.jsx
+++ React/11-event-handling/src/components/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+
+    const [headingText, setHeadingText] = useState("Hello");
+    const [isHovering, setIsHovering] = useState(false);
+
+    function handleClick()  {
+        setHeadingText("Submitted");
+    }
+
+    function handleMouseOver() {
+        setIsHovering(true);
+    }
+
+    function handleMouseOut() {
+        setIsHovering(false);
+    }
+
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
+
+    return (
+        <div className="container">
+            <h1>{headingText}</h1>
+            <input onChange={handleChange} type="text" placeholder="What's your name?" />
+            <button 
+                // style={isHovering && {backgroundColor: "black"}} <-- why does this not work?
+                style={{backgroundColor: isHovering && "black"}}
+                onClick={handleClick}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+            >
+                Submit
+            </button>
+        </div>
+    );
 }
 
 export default App;
