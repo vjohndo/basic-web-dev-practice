816119fb24811cd32fc40865f5aa7ea6ad0d2657
vjohndo <v.john.do@outlook.com>
2023-02-15 18:41:56 +1100

    [S33] Spread operator and state

---
+++ React/15-spread-operator/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["Lime", "Lemon", "Orange"];
+const fruits = ["Apple", "Banana", "Coconut", ...citrus];
+console.log(fruits);
+
+const fullName = {
+  fName: "James",
+  lName: "Bond"
+}
+
+const user = {
+  ...fullName,
+  id: 1,
+  username: "Jamesbond"
+}
+
+console.log(user);
